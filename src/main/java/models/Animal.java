5f19d70a5c53a66c6ea7576b1d8dc11e97f28613
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-07-20 11:41:55 +0300

    added ranger id to animal class

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -3,8 +3,10 @@ package models;
 public class Animal {
     private Integer id;
     private String name;
-    public Animal(String name){
+    private Integer  rangerId;
+    public Animal(String name,int rangerId){
         this.name = name;
+        this.rangerId =rangerId;
     }
 
 }
