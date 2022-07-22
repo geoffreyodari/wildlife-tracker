3ee19baad2f21f0d029542a5bdf97eb69eedd427
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-07-22 11:00:39 +0300

    added static rout to images and styling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
-
+        staticFileLocation("/public");
         //home page
         get("/", (request, response) -> "welcome page");
 
